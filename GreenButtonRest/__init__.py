160ae89aea6df8a029966df35a8aec71248e176a
Eric P <proulx712@gmail.com>
2016-10-01 12:38:37 -0700

    updated the client

---
+++ GreenButtonRest/__init__.py
@@ -0,0 +1,15 @@
+__title__ = 'greenbuttonclient'
+__version__ = '1.0'
+__author__ = 'Eric Proulx'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2016 Eric Proulx'
+
+"""
+greenbutton.client
+~~~~~~~~~~~~
+
+This module implements the GreenButton API.
+
+:copyright: (c) 2016 by Eric Proulx.
+:license: Apache 2.0, see LICENSE for more details.
+"""
